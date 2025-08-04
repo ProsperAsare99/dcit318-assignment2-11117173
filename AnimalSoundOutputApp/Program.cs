5a8513b680062ddd4ed97b63b62940c7ec0842be
PROSPER SHAIBU ASARE <psasare001@st.ug.edu.gh>
2025-08-04 15:43:25 +0000

    animal sound app progress

--- AnimalSoundOutputApp/Program.cs
+++ AnimalSoundOutputApp/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        Console.WriteLine("=== Animal Sounds ===");
+        genericAnimal.MakeSound();  // Outputs: Some generic sound
+        dog.MakeSound();            // Outputs: Bark
+        cat.MakeSound();            // Outputs: Meow
+    }
+}
